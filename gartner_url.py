7adc847c4f2bac8c2082804711281fbc6cea3c1d
lindsaymacvean <lindsaymacvean@gmail.com>
2018-02-07 18:08:49 +0000

    added url builder

--- gartner_url.py
+++ gartner_url.py
@@ -9,14 +9,19 @@ import urllib2
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 print "downloading input csv with urllib2"
 f = urllib2.urlopen('https://docs.google.com/spreadsheets/d/e/2PACX-1vRwSXoVVf6h-LeadAx_gUDCSuUCw_YJSHhdko9jq9qQqis2CW8JXaeIh6aUJzhjgK7rmONbVU_BMSmJ/pub?gid=28561824&single=true&output=csv')
 data = f.read()
 with open("list.csv", "w+") as code:
     code.write(data)
+code.close()
 
-exit(1)
+def wait_for_correct_current_url(desired_url):
+	wait.until(lambda driver: desired_url in driver.current_url)
 
 options = webdriver.ChromeOptions()
 options.binary_location = '/usr/bin/google-chrome'
@@ -42,17 +47,64 @@ if not os.path.isfile('Cookies.pkl'):
     time.sleep(5)
     pickle.dump(driver.get_cookies() , open("Cookies.pkl","wb"))
 
-# Set the Cookies
+print "Set the Cookies"
 driver.get('https://gartner.com')
 for cookie in pickle.load(open("Cookies.pkl", "rb")):
 	driver.add_cookie(cookie)
+driver.get('https://gartner.com')
 
-# Get the Research URL
+print "Get the Research URL"
 for row in inputReader:
-    if row[3] != '':
+	if row[10] != '':
+		outputWriter.writerow(row)
+		continue
+	driver.get(row[11])
+	elem = None
+	try:
+		if elem is None:
+			elem = driver.find_element_by_class_name('linktoSearchQid')
+	except NoSuchElementException:
+		print("Cannot find the url")
+		outputWriter.writerow(row)
+		continue
+	row[10] = elem.get_attribute('href')
+	inputWriter.writerow(row)
+	outputWriter.writerow(row)
+
+inputFile.seek(0)
+outputFile.seek(0)
+print "Get the Document URL"
+for row in inputReader:
+	if row[9] != '':
+		outputWriter.writerow(row)
+		continue
+	url = None
+	try:
+		driver.get(row[10])
+		if url is None:
+			wait = WebDriverWait(driver, 100)
+			desired_url = 'document'
+			wait_for_correct_current_url(desired_url)
+			url = driver.current_url.split("?")[0]
+			print url
+	except Exception as e:
+		print("Cannot find the url")
+		outputWriter.writerow(row)
+		continue
+	row[9] = url
+	inputWriter.writerow(row)
+	outputWriter.writerow(row)
+
+driver.close()
+exit(1)
+
+
+# Get the Document Title
+for row in inputReader:
+    if row[8] != '':
         outputWriter.writerow(row)
         continue
-    driver.get(row[11])
+    driver.get(row[9])
     elem = None
     try:
         if elem is None:
@@ -65,65 +117,4 @@ for row in inputReader:
     inputWriter.writerow(row)
     outputWriter.writerow(row)
 
-exit(1)
-print("\nnow we can check for linkedin URLs")
-inputFile.seek(0)
-outputFile.seek(0)
-for row in inputReader:
-    if row[3] != '':
-        outputWriter.writerow(row)
-        continue
-    driver.get(row[2])
-    try:
-        elem = driver.find_element_by_xpath("//a[contains(@href,'linkedin')]")
-    except NoSuchElementException:
-        print("No Linkedin URL found for "+row[3])
-        row[3] = "NA"
-        outputWriter.writerow(row)
-        continue
-    url = elem.get_attribute('href')
-    row[3] = url
-    inputWriter.writerow(row)
-    outputWriter.writerow(row)
-
-print("\nnow we can check for employee count")
-inputFile.seek(0)
-outputFile.seek(0)
-for row in inputReader:
-    if 4 not in row:
-        row.append('0')
-    if row[3] == 'NA':
-        print("No Linkedin page for "+row[0])
-        outputWriter.writerow(row)
-        continue
-    if row[4] != '0':
-        print(row[0]+" already has "+row[4])
-        outputWriter.writerow(row)
-        continue
-    print(row[3])
-    driver.get(row[3])
-    for cookie in pickle.load(open("Cookies.pkl", "rb")):
-        driver.add_cookie(cookie)
-    try: 
-        elem = driver.find_element_by_xpath("//*[contains(text(), 'See all ') and contains(text(), 'employees on LinkedIn')]")
-    except NoSuchElementException:
-        print("No employee count found")
-        row[4] = "NA"
-        outputWriter.writerow(row)
-        continue
-    string = elem.text.replace(',', '')
-    numbers = [int(s) for s in string.split() if s.isdigit()]
-    employeeCount = numbers[0]
-    row[4] = employeeCount
-    inputWriter.writerow(row)
-    outputWriter.writerow(row)
-
-
-
-#assert "Python" in driver.title
-#elem = driver.find_element_by_name("q")
-#elem.clear()
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
 driver.close()
