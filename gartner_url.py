f5d8b9aa8c9479f631e5336ca4b6b5cedf045a41
lindsay macvean <lindsaymacvean@gmail.com>
2018-02-09 21:04:06 +0000

    added an encoding in utf-8

--- gartner_url.py
+++ gartner_url.py
@@ -20,6 +20,7 @@ documentURL = 9
 researchURL = 10
 analystURL = 11
 archive = 12
+URLBad = 13
 
 print "downloading input csv with urllib2"
 f = urllib2.urlopen('https://docs.google.com/spreadsheets/d/e/2PACX-1vRwSXoVVf6h-LeadAx_gUDCSuUCw_YJSHhdko9jq9qQqis2CW8JXaeIh6aUJzhjgK7rmONbVU_BMSmJ/pub?gid=28561824&single=true&output=csv')
@@ -61,88 +62,84 @@ for cookie in pickle.load(open("Cookies.pkl", "rb")):
 	driver.add_cookie(cookie)
 driver.get('https://gartner.com')
 
-print "Get the Research URL"
-for row in inputReader:
-	if row[researchURL] != '':
-		outputWriter.writerow(row)
-		continue
-	driver.get(row[analystURL])
-	elem = None
-	try:
-		if elem is None:
-			elem = driver.find_element_by_class_name('linktoSearchQid')
-	except NoSuchElementException:
-		print "Cannot find the url"
-		outputWriter.writerow(row)
-		continue
-	row[researchURL] = elem.get_attribute('href')
-	inputWriter.writerow(row)
-	outputWriter.writerow(row)
+# print "Get the Research URL"
+# for row in inputReader:
+# 	if row[researchURL] != '':
+# 		outputWriter.writerow(row)
+# 		continue
+# 	driver.get(row[analystURL])
+# 	elem = None
+# 	try:
+# 		if elem is None:
+# 			elem = driver.find_element_by_class_name('linktoSearchQid')
+# 	except NoSuchElementException:
+# 		print "Cannot find the url"
+# 		outputWriter.writerow(row)
+# 		continue
+# 	row[researchURL] = elem.get_attribute('href')
+# 	inputWriter.writerow(row)
+# 	outputWriter.writerow(row)
 
-inputFile.seek(0)
-outputFile.seek(0)
-print "Get the Document URL"
-for row in inputReader:
-	if row[documentURL] != '':
-		outputWriter.writerow(row)
-		continue
-	url = None
-	try:
-		driver.get(row[researchURL])
-		if url is None:
-			wait = WebDriverWait(driver, 100)
-			desired_url = 'document'
-			wait_for_correct_current_url(desired_url)
-			url = driver.current_url.split("?")[0]
-			print url
-	except Exception as e:
-		print "Cannot find the url"
-		outputWriter.writerow(row)
-		continue
-	row[documentURL] = url
-	inputWriter.writerow(row)
-	outputWriter.writerow(row)
+# inputFile.seek(0)
+# outputFile.seek(0)
+# print "Get the Document URL"
+# for row in inputReader:
+# 	if row[documentURL] != '':
+# 		outputWriter.writerow(row)
+# 		continue
+# 	url = None
+# 	try:
+# 		driver.get(row[researchURL])
+# 		if url is None:
+# 			wait = WebDriverWait(driver, 100)
+# 			desired_url = 'document'
+# 			wait_for_correct_current_url(desired_url)
+# 			url = driver.current_url.split("?")[0]
+# 			print url
+# 	except Exception as e:
+# 		print "Cannot find the url"
+# 		outputWriter.writerow(row)
+# 		continue
+# 	row[documentURL] = url
+# 	inputWriter.writerow(row)
+# 	outputWriter.writerow(row)
 
 
 inputFile.seek(0)
 outputFile.seek(0)
 print "Get the Document Title"
 for row in inputReader:
-	if row[documentTitle] != '':
-		print 'Already has title'
+	if row[documentTitle] != "":
+		print row[documentTitle]+' Already has title'
 		outputWriter.writerow(row)
 		continue
 	try:
 		driver.get(row[documentURL])
 	except Exception as e:
 		print "Cannot find the url"
-		row[documentTitle] = "NA"
-		outputWriter.writerow(row)
-		continue
-	# Check if Analyst created the content
-	fullName = row[foreName].upper()+" "+row[lastName].upper()
-	if (fullName not in driver.page_source.upper()):
-		print fullName+" not found in "+row[documentURL]
-		row[documentTitle] = "Name Wrong"
-		outputWriter.writerow(row)
-		continue
+		row[URLBad] = "Miss"
+
+	# # Check if Analyst created the content
+	# fullName = row[foreName].upper()+" "+row[lastName].upper()
+	# if (fullName not in driver.page_source.upper()):
+	# 	print fullName+" not found in "+row[documentURL]
+	# 	row[URLBad] = "Name Wrong"
 	elem = None
 	try:
 		if elem is None:
-			elem = driver.find_element_by_xpath("//span[contains(@id,'title')]")
-			row[documentTitle] = elem.text
+			elem = driver.find_element_by_xpath("//h1/span[contains(@id,'title')]")
+			row[documentTitle] = elem.text.encode('utf-8')
 	except NoSuchElementException:
-		print "Cannot find the Title"
+		print "Cannot find the Title for "+row[documentURL]
 		row[documentTitle] = "Could not find title"
 		outputWriter.writerow(row)
 		continue
 	# Check if Archived
-	archiveImage = driver.find_elements_by_class_name('archived-btn')
-	if archiveImage:
-		print row[documentTitle]+" Archived"
-		row[archive] = "1"
-	inputWriter.writerow(row)
+	# archiveImage = driver.find_elements_by_class_name('archived-btn')
+	# if archiveImage:
+	# 	print archiveImage
+	# 	print row[documentURL]+" Archived"
+	# 	row[archive] = "1"
 	outputWriter.writerow(row)
 driver.close()
 exit(1)
-	
